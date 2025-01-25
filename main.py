dc2f2f9fc102b16abdfd1328fe3a8bd62bd02f56
LoganG99 <logan.galinier@gmail.com>
2025-01-25 18:00:34 +0100

    count characters

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ def main():
         file_contents = f.read()
     print(file_contents)
     print(how_many_words(file_contents))
+    print(count_characters(file_contents))
 
 def how_many_words(file_contents):
     words = file_contents.split()
     count = len(words)
     return count
+
+def count_characters(file_contents):
+    lowered_string = file_contents.lower()
+    characters = list(lowered_string)
+    dic = {}
+    count = 1
+    for l in characters:
+        if l not in dic:
+            dic[l] = count
+    
+        else:
+            dic[l] += 1
+    return dic
     
     
     
