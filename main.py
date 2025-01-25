a993aa2e1025cd36ab0e1e1caba7949fe35a5690
LoganG99 <logan.galinier@gmail.com>
2025-01-25 15:27:19 +0100

    how many words

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    print(how_many_words(file_contents))
+
+def how_many_words(file_contents):
+    words = file_contents.split()
+    count = len(words)
+    return count
+    
+    
+    
 
 
 
